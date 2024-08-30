20fe6d98407e86a8f3d74210be41cbe44b33967d
silverain27 <116338382+silverain27@users.noreply.github.com>
2024-08-30 10:52:19 +0900

    [level 2] Title: 프로세스, Time: 1.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,11 +1,31 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
 def solution(priorities, location):
-    queue =  [(i,p) for i,p in enumerate(priorities)]
     answer = 0
-    while True:
-        cur = queue.pop(0)
-        if any(cur[1] < q[1] for q in queue):
-            queue.append(cur)
+    from collections import deque
+
+    d = deque([(v,i) for i,v in enumerate(priorities)])
+
+    while len(d):
+        item = d.popleft()
+        if d and max(d)[0] > item[0]:
+            d.append(item)
         else:
             answer += 1
-            if cur[0] == location:
-                return answer
+            if item[1] == location:
+                break
+    return answer
