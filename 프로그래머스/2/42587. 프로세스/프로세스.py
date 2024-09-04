20f35f6074368de51349171e9038f7655732b653
silverain27 <116338382+silverain27@users.noreply.github.com>
2024-09-04 10:36:34 +0900

    [level 2] Title: 프로세스, Time: 0.90 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,31 +1,32 @@
 
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
+# def solution(priorities, location):
+#     answer = 0
+#     from collections import deque
+#     from string import ascii_uppercase
+
+#     index_labels = ascii_uppercase[:len(priorities)]
+#     d = deque([(v, i, index_labels[i]) for i, v in enumerate(priorities)])
+#     print(d)
+#     while len(d):
+#         item = d.popleft()
+#         if d and max(d)[0] > item[0]: # pop했을떄 d확인해줘야함 아무것도 없을수도 있어서
+#             d.append(item)
+#             print(d)
+#         else:
+#             answer += 1
+#             if item[1] == location:
+#                 break
+#     print("answer :", answer)
+#     return answer
+
 def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
     answer = 0
-    from collections import deque
-
-    d = deque([(v,i) for i,v in enumerate(priorities)])
-
-    while len(d):
-        item = d.popleft()
-        if d and max(d)[0] > item[0]:
-            d.append(item)
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
         else:
             answer += 1
-            if item[1] == location:
-                break
-    return answer
+            if cur[0] == location:
+                return answer
