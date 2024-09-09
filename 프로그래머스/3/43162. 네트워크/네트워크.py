1f39755d7327881406d4e8bdc77d3227248937bd
silverain27 <116338382+silverain27@users.noreply.github.com>
2024-09-09 11:32:34 +0900

    [level 3] Title: 네트워크, Time: 0.85 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,16 @@
+def dfs(n,computers, start, visited):
+    visited[start] = True
+    for i in range(0,n):
+        if(visited[i]==False and computers[start][i]==1):
+            dfs(n,computers,i,visited)
+            
+
+            
+def solution(n,computers):
+    visited = [False]*n
+    answer = 0
+    for start in range(0,n):
+        if(visited[start]==False):
+            dfs(n,computers,start,visited)
+            answer +=1
+    return answer
