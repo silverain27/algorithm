a88519905cb3321c07e1e7bf547fa6086e80eeb5
silverain27 <116338382+silverain27@users.noreply.github.com>
2024-09-12 14:29:42 +0900

    [level 3] Title: 네트워크, Time: 1.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,41 @@
-def DFS(i, visited, n, computers):
-        visited[i] = 1
-        for a in range(n):
-            if computers[i][a] and not visited[a]:
-                DFS(a, visited, n, computers)    
+# def DFS(i, visited, n, computers):
+#         visited[i] = 1
+#         for a in range(n):
+#             if computers[i][a]==1 and not visited[a]:
+#                 DFS(a, visited, n, computers)    
                 
-def solution(n, computers):            
-    
-           
+# def solution(n, computers):                  
+#     answer = 0
+#     visited = [False] * n 
+#     # 뭉탱이는 시작점이 없으니까 모든 컴퓨터를 다 본다 
+#     for i in range(n):
+#         if not visited[i]: #
+#             DFS(i, visited, n, computers)
+#             answer += 1 # 뭉탱이 끝나면 count 해주기 
+        
+#     return answer
+
+from collections import deque
+
+def bfs(n, start, visited, computers):
+        visited[start] = True
+        queue = deque([start])
+        while queue:
+            v = queue.popleft()
+            for i in range(n):
+                # 방문하지 않은 연결된 컴퓨터
+                if computers[v][i] == 1 and not visited[i]:
+                    visited[i] = True
+                    queue.append(i)
+                    
+def solution(n, computers):
     answer = 0
-    visited = [0 for i in range(len(computers))]
+    visited = [False] * n
+
+    # 방문하지 않은 컴퓨터 중 작은 번호부터 BFS 수행
     for i in range(n):
         if not visited[i]:
-            DFS(i, visited, n, computers)
+            bfs(n, i, visited, computers)
             answer += 1
-        
+
     return answer
